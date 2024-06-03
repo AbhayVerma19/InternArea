cb27baadb245a3a87f0a5afd3e3392114848c311
Abhay Verma <abhver192002@gmail.com>
2024-06-03 10:40:42 +0530

    Firebase authentication and home page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      </div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
   );
 }
 
