5798244a4c7f80a65472bfe3436ea7b78885abb8
Abhay Verma <abhver192002@gmail.com>
2024-06-06 15:41:55 +0530

    Using .env file for db authentication

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const mongoose=require("mongoose")
-//require('dotenv').config()
-// DATABASE=process.env.DATABASEURL
-const url=`mongodb+srv://abhver192002:Abhayverma19@cluster0.ael5vfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+require('dotenv').config()
+DATABASE=process.env.DATABASEURL
 module.exports.connect=()=>{
-    mongoose.connect(url,console.log("Database is connected"))
+    mongoose.connect(DATABASE,console.log("Database is connected"))
 }
